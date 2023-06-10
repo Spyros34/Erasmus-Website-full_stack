77d90f978c27e2db93f9d6489dde13775c59b6b0
Spyros Konidas <57417722+Spyros34@users.noreply.github.com>
2023-06-10 18:23:17 +0300

    Add files via upload

--- post_signup_data.php
+++ post_signup_data.php
@@ -1,5 +1,5 @@
 <?php
-
+    session_start();
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
     //receiving all the elements from the sign up form
@@ -21,31 +21,56 @@
     }else {
         mysqli_select_db($con, $db_name);
 
-        // Prepare and execute the SELECT query
-        $query_1="INSERT INTO Users (fname,lname,email,username,password,a_m,tel) VALUES('$fname','$lname','$email','$username','$password','$a_m','$tel')";
-        $result=mysqli_query($con,$query_1);
+        //Check if the username already exists 
+        $query= "SELECT COUNT(*) as count FROM users WHERE username = '$username'";
+        $result=mysqli_query($con,$query);
+        $row = $result->fetch_assoc();
+        $count = $row['count'];
+         
+        if ($count > 0) {
+            echo "Username already exists. Cannot insert.";
+        }else {    //the username doesn't exist 
 
-        if ($result)
-        {
-            $user_id = mysqli_insert_id($con);
+            // Prepare and execute the insert query
+            $query_1="INSERT INTO Users (fname,lname,email,username,password,a_m,tel) VALUES('$fname','$lname','$email','$username','$password','$a_m','$tel')";
+            $result=mysqli_query($con,$query_1);
 
-        }else {
-            //error 
-        }
-        $query_1="INSERT INTO User_t (user_type_id,user_type) VALUES('$user_id','0')";
-        $result=mysqli_query($con,$query_1);
-    
+            if ($result)
+            {
+                $user_id = mysqli_insert_id($con);  //get the user_id of the inserted user 
+
+            }else {
+                //error 
+            }
+
+            $query_1="INSERT INTO User_t (user_type_id,user_type) VALUES('$user_id','0')"; // user_type = 0 for registered user ,  user_type = 1 for administrator user
+            $result=mysqli_query($con,$query_1);
         
-        if ($con->query($query_1) === TRUE) {
-            echo "Data inserted successfully";
-        } else {
-            echo "Error inserting data: " . mysqli_close($con);;
+            
+            if ($con->query($query_1) === TRUE) {
+                echo "Data inserted successfully";
+            } else {
+                echo "Error inserting data: " . mysqli_close($con);;
+            }
+            
+            // Close the database connection
+            mysqli_close($con); 
+            
+            //session 
+            $_SESSION["username"] = $username ;
+            
+            //coockies 
+            //set a cookie for 30 days and the destination of the cookie is "/"
+            setcookie("username", $username, time() + (86400 * 30), "/");
+            setcookie("password", $password, time() + (86400 * 30), "/");
+            setcookie("fname", $fname, time() + (86400 * 30), "/");
+            setcookie("lname", $lname, time() + (86400 * 30), "/");
+            setcookie("email", $email, time() + (86400 * 30), "/");
+            setcookie("a_m", $a_m, time() + (86400 * 30), "/");
+            setcookie("tel", $tel, time() + (86400 * 30), "/");
+
         }
-        
-        // Close the database connection
-        mysqli_close($con); 
-   
- 
+
     }
     
 
