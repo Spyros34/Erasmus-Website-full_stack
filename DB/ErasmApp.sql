ac948d85f53eb6828dfbcbf4d7f06b04a0d2f77f
Spyros Konidas <57417722+Spyros34@users.noreply.github.com>
2024-02-15 19:33:01 +0200

    Add files via upload

--- DB/ErasmApp.sql
+++ DB/ErasmApp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Εξυπηρετητής: localhost
--- Χρόνος δημιουργίας: 15 Ιουν 2023 στις 23:11:53
+-- Χρόνος δημιουργίας: 15 Φεβ 2024 στις 18:32:20
 -- Έκδοση διακομιστή: 10.4.28-MariaDB
 -- Έκδοση PHP: 8.2.4
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Βάση δεδομένων: `ErasmApp`
+-- Βάση δεδομένων: `GithubTest`
 --
 
 -- --------------------------------------------------------
@@ -47,29 +47,7 @@ CREATE TABLE `applications` (
 --
 
 INSERT INTO `applications` (`application_id`, `passed_courses`, `average_passed_courses`, `english_certificate`, `foreign_languages`, `first_uni`, `second_uni`, `third_uni`, `transcript`, `english_degree`, `other_degrees`, `tick_box`) VALUES
-(4, 70, 70, 'B2', 'ΝΟ', '1', '1', '1', 'files/theodor/Εργασία 3.pdf', 'files/theodor/- (4).pdf', 'files/theodor/test.pdf,files/theodor/01. logic (3).pdf', 0),
-(5, 70, 80, 'C1', 'Yes', 'University of Barcelona', 'University of Vienna', 'Hanze University', 'files/theodor/Εργασία 3.pdf', 'files/theodor/ErasmApp.sql', 'files/theodor/- (4).pdf,files/theodor/Εργαστήριο_Γραφείο_Διασύνδεσης_Ανακοίνωση_v2.pdf,files/theodor/index.html,files/theodor/test.pdf', 0),
-(6, 70, 70, 'B2', 'Yes', 'University of Vienna', 'University of Vienna', 'Hanze University', 'files/test/Εργασία 3.pdf', 'files/test/ErasmApp.sql', 'files/test/- (4).pdf,files/test/Εργαστήριο_Γραφείο_Διασύνδεσης_Ανακοίνωση_v2.pdf,files/test/index.html,files/test/test.pdf', 0);
-
--- --------------------------------------------------------
-
---
--- Δομή πίνακα για τον πίνακα `dates`
---
-
-CREATE TABLE `dates` (
-  `date_from` date DEFAULT NULL,
-  `date_to` date DEFAULT NULL,
-  `enable` int(11) DEFAULT NULL,
-  `date_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
---
--- Άδειασμα δεδομένων του πίνακα `dates`
---
-
-INSERT INTO `dates` (`date_from`, `date_to`, `enable`, `date_id`) VALUES
-('2023-06-16', '2023-06-22', 0, 1);
+(12, 80, 70, 'A2', 'Yes', 'University of Barcelona', 'Hanze University', 'University of Vienna', 'files/spyros/grades.pdf', 'files/spyros/english_degree.pdf', 'files/spyros/degree1.pdf,files/spyros/degree2.pdf', 1);
 
 -- --------------------------------------------------------
 
@@ -87,9 +65,7 @@ CREATE TABLE `fill` (
 --
 
 INSERT INTO `fill` (`user_id`, `application_id`) VALUES
-(13, 4),
-(13, 5),
-(10, 6);
+(13, 12);
 
 -- --------------------------------------------------------
 
@@ -134,14 +110,8 @@ CREATE TABLE `Users` (
 --
 
 INSERT INTO `Users` (`user_id`, `fname`, `lname`, `password`, `username`, `email`, `a_m`, `tel`) VALUES
-(1, 'nick', 'tselikas', '1234', 'nick_tsel', 'nicktsel@gmail.com', '20222020202020', '6969696969'),
-(8, 'Spyros', 'Kon', '12345!', 'SpyrosK', 'spyrokonida@gmail.com', '2022374859438', '6947418946'),
-(9, 'Makis', 'kotsampasis', '12345!', 'makiskots1', 'spyrokonida@gmail.com', '2022394756478', '6947418946'),
-(10, 'test', 'test', '12345!', 'test', 'spyrokonida@gmail.com', '2022348576934', '6947418946'),
-(11, 'something ', 'some', '12345!', 'some', 'spyrokonida@gmail.com', '2022384567584', '6947418946'),
-(12, 'jay', 'jay', '12345!', 'jay', 'spyrokonida@gmail.com', '2022394857485', '6947418946'),
-(13, 'thodoris', 'maran', '1234!', 'theodor', 'theodor@gmail.com', '2022394857438', '6947418946'),
-(15, 'admin', 'admin', '12345!', 'admin', 'dkkdj@gmail.com', '2022348576940', '6947418947');
+(13, 'Spyros', 'Konidas', '1234!', 'Spyros', 'spyros@gmail.com', '2022354757438', '6900000000'),
+(15, 'admin', 'admin', '12345!', 'admin', 'dkkdj@gmail.com', '2022948676940', '6900000000');
 
 -- --------------------------------------------------------
 
@@ -159,15 +129,6 @@ CREATE TABLE `User_t` (
 --
 
 INSERT INTO `User_t` (`user_type_id`, `user_type`) VALUES
-(8, 0),
-(9, 0),
-(9, 0),
-(10, 0),
-(10, 0),
-(11, 0),
-(11, 0),
-(12, 0),
-(12, 0),
 (13, 0),
 (13, 0),
 (15, 1),
@@ -182,12 +143,6 @@ INSERT INTO `User_t` (`user_type_id`, `user_type`) VALUES
 --
 ALTER TABLE `applications`
   ADD PRIMARY KEY (`application_id`);
-
---
--- Ευρετήρια για πίνακα `dates`
---
-ALTER TABLE `dates`
-  ADD PRIMARY KEY (`date_id`);
 
 --
 -- Ευρετήρια για πίνακα `fill`
@@ -222,13 +177,7 @@ ALTER TABLE `User_t`
 -- AUTO_INCREMENT για πίνακα `applications`
 --
 ALTER TABLE `applications`
-  MODIFY `application_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT για πίνακα `dates`
---
-ALTER TABLE `dates`
-  MODIFY `date_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `application_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT για πίνακα `Universities`
